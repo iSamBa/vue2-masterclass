d7b4abe2644b163187a05c2fa5d6eeebbbccac9c
iSamBa <bahou.aissam@gmail.com>
2021-03-27 22:28:42 +0100

    run npm run lint --fix

--- getting-started/.eslintrc.js
+++ getting-started/.eslintrc.js
@@ -6,14 +6,14 @@ module.exports = {
   },
 
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
 
   rules: {
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
 
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/standard"]
-};
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/standard']
+}
