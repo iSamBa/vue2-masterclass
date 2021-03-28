1407189f56249150b51269df9897cacec90db454
iSamBa <bahou.aissam@gmail.com>
2021-03-28 13:37:43 +0200

    create a Not Found page

--- getting-started/src/router/index.js
+++ getting-started/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
+import PageNotFound from '@/components/PageNotFound.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: PageHome },
@@ -9,7 +10,8 @@ const routes = [
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true
-  }
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound }
 ]
 
 export default createRouter({
