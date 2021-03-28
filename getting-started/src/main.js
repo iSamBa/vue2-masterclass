72f52fadb25986e23e370e2ee4e15a3ccda20a23
iSamBa <bahou.aissam@gmail.com>
2021-03-28 12:34:11 +0200

    add PageThreadShow component

--- getting-started/src/main.js
+++ getting-started/src/main.js
@@ -2,8 +2,17 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import PageHome from '@/components/PageHome.vue'
+import PageThreadShow from '@/components/PageThreadShow.vue'
 
-const routes = [{ path: '/', name: 'Home', component: PageHome }]
+const routes = [
+  { path: '/', name: 'Home', component: PageHome },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
