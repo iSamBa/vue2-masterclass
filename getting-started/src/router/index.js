87c8bdbe6430b9e61dc519550c712be9502c4462
iSamBa <bahou.aissam@gmail.com>
2021-03-28 21:35:22 +0200

    create Forum component

--- getting-started/src/router/index.js
+++ getting-started/src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import ThreadShow from '@/pages/ThreadShow.vue'
 import NotFound from '@/pages/NotFound.vue'
+import Forum from '@/pages/Forum.vue'
 import sourceData from '@/data.json'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
+  { path: '/forum/:id', name: 'Forum', component: Forum, props: true },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
