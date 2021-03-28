a3509e56537a1058cf6eda8696a8609c55607d36
iSamBa <bahou.aissam@gmail.com>
2021-03-28 20:28:19 +0200

    install and use dayjs for relative time

--- getting-started/src/components/PostList.vue
+++ getting-started/src/components/PostList.vue
@@ -20,8 +20,8 @@
           </div>
         </div>
 
-        <div class="post-date text-faded">
-          {{post.publishedAt}}
+        <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
+          {{diffForHuman(post.publishedAt)}}
         </div>
       </div>
     </div>
@@ -29,6 +29,12 @@
 
 <script>
 import sourceData from '@/data.json'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedDate from 'dayjs/plugin/localizedFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
+
 export default {
   name: 'PostList',
   props: {
@@ -43,9 +49,14 @@ export default {
     }
   },
   methods: {
-
     userById (userId) {
       return this.users.find(p => p.id === userId)
+    },
+    diffForHuman (timestamp) {
+      return dayjs.unix(timestamp).fromNow()
+    },
+    humanFriendlyDate (timestamp) {
+      return dayjs.unix(timestamp).format('llll')
     }
   }
 
