fe767da9adf6b87726905f4593ca088040dde8bd
iSamBa <bahou.aissam@gmail.com>
2021-03-28 14:08:33 +0200

    add route guards for not existing threads

--- getting-started/src/router/index.js
+++ getting-started/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   { path: '/', name: 'Home', component: PageHome },
@@ -9,7 +10,25 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if the thread exists
+      const threadExists = sourceData.threads.find(
+        thread => thread.id === to.params.id
+      )
+      if (threadExists) {
+        // Move to next
+        return next()
+      } else {
+        // Thread does not exist ==> Redirect to Not found
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          hash: to.hash,
+          query: to.query
+        })
+      }
+    }
   },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound }
 ]
