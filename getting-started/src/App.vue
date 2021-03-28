505f641df3754e6482cba4b01161af01623a526b
iSamBa <bahou.aissam@gmail.com>
2021-03-28 12:27:40 +0200

    install and configure vue router

--- getting-started/src/App.vue
+++ getting-started/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div class="container">
-    <page-home/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-
-import PageHome from './components/PageHome.vue'
 export default {
   name: 'App',
   components: {
-    PageHome
+
   }
 }
 </script>
--- getting-started/src/main.js
+++ getting-started/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import PageHome from '@/components/PageHome.vue'
+
+const routes = [{ path: '/', name: 'Home', component: PageHome }]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const application = createApp(App)
+application.use(router)
 application.mount('#app')
