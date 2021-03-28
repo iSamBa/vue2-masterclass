b6034c1a46b1094809368bd196e128845539e7d6
iSamBa <bahou.aissam@gmail.com>
2021-03-28 12:37:36 +0200

    refactor router to a specific folder

--- getting-started/src/main.js
+++ getting-started/src/main.js
@@ -1,23 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import PageHome from '@/components/PageHome.vue'
-import PageThreadShow from '@/components/PageThreadShow.vue'
-
-const routes = [
-  { path: '/', name: 'Home', component: PageHome },
-  {
-    path: '/thread/:id',
-    name: 'ThreadShow',
-    component: PageThreadShow,
-    props: true
-  }
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+import router from '@/router'
 
 const application = createApp(App)
 application.use(router)
