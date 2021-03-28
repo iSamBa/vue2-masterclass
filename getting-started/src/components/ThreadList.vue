84a2c92e63b09604b21863e4d04b1d24b64d15c3
iSamBa <bahou.aissam@gmail.com>
2021-03-28 20:52:39 +0200

    make global automatically all components starting with App

--- getting-started/src/components/ThreadList.vue
+++ getting-started/src/components/ThreadList.vue
@@ -37,9 +37,7 @@
 
 <script>
 import sourceData from '@/data.json'
-import AppDate from '@/components/AppDate.vue'
 export default {
-  components: { AppDate },
   props: {
     threads: {
       type: Array,
--- getting-started/src/main.js
+++ getting-started/src/main.js
@@ -4,4 +4,17 @@ import router from '@/router'
 
 const application = createApp(App)
 application.use(router)
+const requireComponent = require.context(
+  './components',
+  true,
+  /App[A-Z]\w+\.(vue|js)$/
+)
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName =
+    baseComponentConfig.name ||
+    fileName.replace(/^.+\//, '').replace(/\.\w+$/, '')
+  application.component(baseComponentName, baseComponentConfig)
+})
 application.mount('#app')
